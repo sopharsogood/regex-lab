6352c2d3596a1a9e44a49d36e5a3ca3b1dd6fb94
sopharsogood <justanothersophie@gmail.com>
2021-07-22 11:08:50 -0400

    commit tree 0af4a358dc36e9090c2aa8db0125f1748c973dab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/\A[A-Z].*[.,<>?\/\\:;"'{}\[\]\(\)]\z/)
+  # wasn't sure how many punctuations to include
 end
 
 def valid_phone_number?(phone)
-
+  !!phone.match(/(\d{3}\W?){2}\d{4}/)
 end
